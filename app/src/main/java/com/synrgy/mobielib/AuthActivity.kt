b659d018330edd48406ced3c2d9e30551871af90
azaiskr <nuraziss557@gmail.com>
2024-06-24 21:50:01 +0700

    bug fix

--- app/src/main/java/com/synrgy/mobielib/AuthActivity.kt
+++ app/src/main/java/com/synrgy/mobielib/AuthActivity.kt
@@ -11,12 +11,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_auth)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
